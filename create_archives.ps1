ff82453e93b29c3aa8469dfafd5e849e7b5be465
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2023-12-09 03:24:38 -0500

    fix build script

--- create_archives.ps1
+++ create_archives.ps1
@@ -1,7 +1,10 @@
+Write-Host "Creating archives..."
 Copy-Item -Path .\straycat_server.py -Destination .\publish\
 Copy-Item -Path .\straycat_server.py -Destination .\publish\mac\
 Copy-Item -Path .\straycat_server.py -Destination .\publish\linux\
-
+Write-Host "Creating Windows archive..."
 Compress-Archive -Path .\publish\* -DestinationPath .\publish\StrayCatRunner_Windows.zip
+Write-Host "Creating Mac archive..."
 Compress-Archive -Path .\publish\mac\* -DestinationPath .\publish\StrayCatRunner_Mac.zip
+Write-Host "Creating Linux archive..."
 Compress-Archive -Path .\publish\linux\* -DestinationPath .\publish\StrayCatRunner_Linux.zip
